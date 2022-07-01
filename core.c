5ee663ff625952f10e8db08465392f1c256e37a1
Alba Mendez <me@alba.sh>
2022-07-01 20:12:00 +0200

    reorder sections

--- core.c
+++ core.c
@@ -72,55 +72,7 @@ uint32_t __core_read_rs2(const core_t* core, uint32_t instr) {
 #define __negBit (instr & (1 << 30))
 
 
-// INSTRUCTIONS
-
-uint32_t __core_int_op(uint32_t instr, bool isReg, uint32_t a, uint32_t b) {
-    switch (__core_dec_func3(instr)) {
-        case RISCV_IFUNC_ADD:  return a + ((isReg && __negBit) ? -b : b);
-
-        case RISCV_IFUNC_SLT:  return ((int32_t)a) < ((int32_t)b);
-        case RISCV_IFUNC_SLTU: return a < b;
-
-        case RISCV_IFUNC_XOR:  return a ^ b;
-        case RISCV_IFUNC_OR:   return a | b;
-        case RISCV_IFUNC_AND:  return a & b;
-
-        case RISCV_IFUNC_SLL:  return a << (b & MASK(5));
-        case RISCV_IFUNC_SRL:  return __negBit ? (uint32_t)(((int32_t)a) >> (b & MASK(5))) : // SRA
-                                                                      a  >> (b & MASK(5))  ; // SRL
-    }
-    __builtin_unreachable();
-}
-
-bool __core_jmp_op(core_t* core, uint32_t instr, uint32_t a, uint32_t b) {
-    switch (__core_dec_func3(instr)) {
-        case RISCV_BFUNC_BEQ:  return a == b;
-        case RISCV_BFUNC_BNE:  return a != b;
-        case RISCV_BFUNC_BLTU: return a  < b;
-        case RISCV_BFUNC_BGEU: return a >= b;
-        case RISCV_BFUNC_BLT:  return ((int32_t)a)  < ((int32_t)b);
-        case RISCV_BFUNC_BGE:  return ((int32_t)a) >= ((int32_t)b);
-    }
-    core->error = ERR_BAD_INSTRUCTION;
-    return false;
-}
-
-void __core_set_dest(core_t* core, uint32_t instr, uint32_t x) {
-    uint8_t rd = __core_dec_rd(instr);
-    if (rd) core->x_regs[rd] = x;
-}
-
-void __core_jump(core_t* core, uint32_t addr) {
-    if (unlikely(addr & 0b11))
-        core->error = ERR_JUMP_UNALIGNED;
-    else
-        core->pc = addr;
-}
-
-void __core_jump_link(core_t* core, uint32_t instr, uint32_t addr) {
-    __core_set_dest(core, instr, core->pc); // FIXME: if the jump fails (misalign), does rd need to be updated?
-    __core_jump(core, addr);
-}
+// CSR INSTRUCTIONS
 
 #define CSR_BITFIELD_CASE(R, W, CASE, CODE) \
     case (CASE): \
@@ -232,6 +184,57 @@ void __core_do_system(core_t* core, uint32_t instr) {
         default:
             core->error = ERR_BAD_INSTRUCTION; return;
     }
+}
+
+
+// UNPRIVILEGED INSTRUCTIONS
+
+uint32_t __core_int_op(uint32_t instr, bool isReg, uint32_t a, uint32_t b) {
+    switch (__core_dec_func3(instr)) {
+        case RISCV_IFUNC_ADD:  return a + ((isReg && __negBit) ? -b : b);
+
+        case RISCV_IFUNC_SLT:  return ((int32_t)a) < ((int32_t)b);
+        case RISCV_IFUNC_SLTU: return a < b;
+
+        case RISCV_IFUNC_XOR:  return a ^ b;
+        case RISCV_IFUNC_OR:   return a | b;
+        case RISCV_IFUNC_AND:  return a & b;
+
+        case RISCV_IFUNC_SLL:  return a << (b & MASK(5));
+        case RISCV_IFUNC_SRL:  return __negBit ? (uint32_t)(((int32_t)a) >> (b & MASK(5))) : // SRA
+                                                                      a  >> (b & MASK(5))  ; // SRL
+    }
+    __builtin_unreachable();
+}
+
+bool __core_jmp_op(core_t* core, uint32_t instr, uint32_t a, uint32_t b) {
+    switch (__core_dec_func3(instr)) {
+        case RISCV_BFUNC_BEQ:  return a == b;
+        case RISCV_BFUNC_BNE:  return a != b;
+        case RISCV_BFUNC_BLTU: return a  < b;
+        case RISCV_BFUNC_BGEU: return a >= b;
+        case RISCV_BFUNC_BLT:  return ((int32_t)a)  < ((int32_t)b);
+        case RISCV_BFUNC_BGE:  return ((int32_t)a) >= ((int32_t)b);
+    }
+    core->error = ERR_BAD_INSTRUCTION;
+    return false;
+}
+
+void __core_set_dest(core_t* core, uint32_t instr, uint32_t x) {
+    uint8_t rd = __core_dec_rd(instr);
+    if (rd) core->x_regs[rd] = x;
+}
+
+void __core_jump(core_t* core, uint32_t addr) {
+    if (unlikely(addr & 0b11))
+        core->error = ERR_JUMP_UNALIGNED;
+    else
+        core->pc = addr;
+}
+
+void __core_jump_link(core_t* core, uint32_t instr, uint32_t addr) {
+    __core_set_dest(core, instr, core->pc); // FIXME: if the jump fails (misalign), does rd need to be updated?
+    __core_jump(core, addr);
 }
 
 void core_step(core_t* core) {
